90f3da41e0abe5d9ec71f47433c8f4d2e12b5fb9
MohammadReza Shekohi <mrshekohi079@gmail.com>
2024-10-29 23:42:04 +0330

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Converter from './components/Converter.vue';
+
+
+</script>
+
+<template>
+    <main class="flex justify-center items-center h-screen bg-gradient-to-l from-blue-200 to-blue-300">
+        <Converter />
+    </main>
+    <footer class="bg-slate-800 text-slate-50 py-2 text-center font-thin">
+        Project 4 FUN
+    </footer>
+</template>
---
+++ src/components/Converter.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref } from 'vue';
+
+const input = ref("");
+const inputBase = ref(10);
+const outputBase = ref(2);
+const numberBases = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+const showOutput = ref(false);
+
+const inputRes = ref("");
+const output = ref("");
+const inputBaseRes = ref(null);
+const outputBaseRes = ref(null);
+
+const convertBase = () => {
+    const baseTenNumber = Number.parseInt(input.value, inputBase.value);
+    output.value = baseTenNumber.toString(outputBase.value).toUpperCase();
+    inputRes.value = input.value;
+    inputBaseRes.value = inputBase.value;
+    outputBaseRes.value = outputBase.value;
+    showOutput.value = true;
+}
+</script>
+
+<template>
+    <div class="w-full flex flex-col items-center">
+        <h3 class="text-center mb-4 text-2xl font-black text-blue-800">Number Base Converter</h3>
+        <div class="px-6 py-8 bg-slate-50 rounded-lg w-[90%] lg:w-[800px] shadow-lg">
+            <form @submit.prevent="convertBase">
+                <div class="flex flex-col md:flex-row gap-x-10 gap-y-4">
+                    <label class="w-full">
+                        Number:
+                        <input v-model="input" class="border border-slate-300 w-full px-2 py-2 rounded-md"
+                            placeholder="Enter a number to be converted" />
+                    </label>
+
+                    <div class="flex gap-x-6 w-full lg:w-1/3">
+                        <div class="w-full">
+                            <label class="flex flex-col">
+                                From:
+                                <select v-model="inputBase"
+                                    class="outline-none border border-slate-300 p-2 rounded-md w-full">
+                                    <option v-for="nb in numberBases" :key="nb" :value="nb">{{ nb }}</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div class="w-full">
+                            <label class="flex flex-col">
+                                To:
+                                <select v-model="outputBase"
+                                    class="outline-none border border-slate-300 p-2 rounded-md w-full">
+                                    <option v-for="nb in numberBases" :key="nb" :value="nb">{{ nb }}</option>
+                                </select>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <button type="submit" class="bg-blue-500 rounded-md py-2 text-slate-50 w-full mt-10">Convert</button>
+            </form>
+
+            <div v-if="showOutput" class="w-full border-t-[0.2px] border-gray-400 my-6"></div>
+
+            <div v-if="showOutput" class="flex gap-4 justify-center items-center">
+                <span class="text-xl">({{ inputRes }})<sub>{{ inputBaseRes }}</sub></span>
+                <span class="text-gray-600">Converted to:</span>
+                <span class="text-xl">({{ output }})<sub>{{ outputBaseRes }}</sub></span>
+            </div>
+        </div>
+    </div>
+</template>
